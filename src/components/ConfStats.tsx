cccb41a0aa9b9d02eba64526b2939ff1f1aa7a29
Camille Moussu <ca.noiray@gmail.com>
2024-06-25 11:45:27 +0400

    [#14] moved conf stats and added summary

--- src/components/ConfStats.tsx
+++ src/components/ConfStats.tsx
@@ -15,46 +15,51 @@ export function ConfStats({ config }: { config: llngConfig }) {
       ? Object.keys(config.casAppMetaDataOptions).length
       : 0);
   return (
-    <div className="main">
-      <div className="box" style={{ maxWidth: "350px" }}>
-        <strong>
-          {t("appNum")} ({appNum})
-        </strong>
-        <PieChart
-          paddingAngle={18}
-          data={[
-            {
-              title: t("saml"),
-              value: config.samlSPMetaDataXML
-                ? Object.keys(config.samlSPMetaDataXML).length
-                : 0,
-              color: "#E38627",
-            },
-            {
-              title: t("oidc"),
-              value: config.oidcRPMetaDataOptions
-                ? Object.keys(config.oidcRPMetaDataOptions).length
-                : 0,
-              color: "#C13C37",
-            },
-            {
-              title: t("cas"),
-              value: config.casAppMetaDataOptions
-                ? Object.keys(config.casAppMetaDataOptions).length
-                : 0,
-              color: "#6A2135",
-            },
-          ]}
-          label={({ dataEntry }) => dataEntry.title + " : " + dataEntry.value}
-          labelStyle={() => ({
-            fontSize: "5px",
-            fontFamily: "sans-serif",
-          })}
-          lineWidth={30}
-          labelPosition={110}
-          radius={30}
-        />
-      </div>
+    <div>
+      <strong>
+        {t("appNum")} ({appNum})
+      </strong>
+      <PieChart
+        paddingAngle={15}
+        data={[
+          {
+            title: t("saml"),
+            value: config.samlSPMetaDataXML
+              ? Object.keys(config.samlSPMetaDataXML).length
+              : 0,
+            color: "#E38627",
+          },
+          {
+            title: t("virtualHost"),
+            value: config.locationRules
+              ? Object.keys(config.locationRules).length
+              : 0,
+            color: "#d65600",
+          },
+          {
+            title: t("oidc"),
+            value: config.oidcRPMetaDataOptions
+              ? Object.keys(config.oidcRPMetaDataOptions).length
+              : 0,
+            color: "#C13C37",
+          },
+          {
+            title: t("cas"),
+            value: config.casAppMetaDataOptions
+              ? Object.keys(config.casAppMetaDataOptions).length
+              : 0,
+            color: "#6A2135",
+          },
+        ]}
+        label={({ dataEntry }) => dataEntry.title + " : " + dataEntry.value}
+        labelStyle={() => ({
+          fontSize: "5px",
+          fontFamily: "sans-serif",
+        })}
+        lineWidth={30}
+        labelPosition={110}
+        radius={20}
+      />
     </div>
   );
 }
